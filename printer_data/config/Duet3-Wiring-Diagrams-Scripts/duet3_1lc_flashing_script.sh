f51409d3867b5e82140a218b922220b4d0059b96
DrFate09 <drfateboy@gmail.com>
2024-08-14 16:09:34 -0400

    Update duet3_1lc_flashing_script.sh

--- printer_data/config/Duet3-Wiring-Diagrams-Scripts/duet3_1lc_flashing_script.sh
+++ printer_data/config/Duet3-Wiring-Diagrams-Scripts/duet3_1lc_flashing_script.sh
@@ -7,7 +7,7 @@
 # 4.  Connect GPIO pin 25 on the Pi to the SWCLK (purple) pin on the board.
 # 5.  Connect GPIO pin 24 on the Pi to the SWDIO (blue) pin on the board.
 # 6.  Connect GPIO pin 18 on the Pi to the SRST (white) pin on the board.
-# 7.  Connect a ground (black) pin on the Pi to ground on the board.
+# 7.  Connect a ground pin on the Pi to ground (black) on the board.
 # 8.  Power on the Pi.
 # 9.  Power on the board (you may do this by supplying power from the Pi (grey)).
 # 10. Build klipper by running make.
